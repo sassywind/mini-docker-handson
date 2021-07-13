6ad1ede69804855b50aa21d98aa874e568ad059c
arata.sashida <art52wind@gmail.com>
2021-07-14 08:22:52 +0900

    lesson1の子プロセスの呼び出し時にプロセスIDの名前空間のみ渡すように変更

--- lessons/lesson1/run.py
+++ lessons/lesson1/run.py
@@ -9,10 +9,7 @@ def pre_exec(arg1: str, arg2: int):
 
 def exec_run():
     flags = (
-            linux.CLONE_NEWPID |  # PID名前空間: プロセスIDの分離。異なる名前空間同士では、同一のプロセスIDを持つことが可能になる
-            linux.CLONE_NEWUTS |  # UTS名前空間: ホスト名, ドメイン名の分離
-            linux.CLONE_NEWNS |  # マウント名前空間: ファイルシステムのマウントポイントの分離
-            linux.CLONE_NEWNET  # ネットワーク名前空間: 分離されたネットワークスタックを提供する
+        linux.CLONE_NEWPID # PID名前空間: プロセスIDの分離。異なる名前空間同士では、同一のプロセスIDを持つことが可能になる
     )
 
     print(f'run command called!')
